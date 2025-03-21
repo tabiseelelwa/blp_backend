02eb6e1caa1e7c9e705a313d3339636d08312ba4
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2025-03-21 13:13:38 +0100

    validation des variables d'environnement

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-const origin = [process.env.ORIGIN_URL || "https://fizitech.org"];
+const origin = [process.env.ORIGIN_URL];
 
 app.use(
   cors({
